8f6bb6f45bc678cda82e0b35edb5c79034fc7d1f
Henry <henrygustavof@gmail.com>
2019-09-21 14:26:13 -0500

    logout

--- lib/main.dart
+++ lib/main.dart
@@ -26,26 +26,25 @@ class MyApp extends StatelessWidget {
     return StoreProvider(
         store: store,
         child: MaterialApp(
-            title: 'Flutter E-Commerce',
-            routes: {
-              '/products': (BuildContext context) => ProductsPage(onInit: () {
-                    StoreProvider.of<AppState>(context).dispatch(getUserAction);
-                    StoreProvider.of<AppState>(context)
-                        .dispatch(getProductsAction);
-                  }),
-              '/login': (BuildContext context) => LoginPage(),
-              '/register': (BuildContext context) => RegisterPage()
-            },
-            theme: ThemeData(
-                brightness: Brightness.dark,
-                primaryColor: Colors.cyan[400],
-                accentColor: Colors.deepOrange[200],
-                textTheme: TextTheme(
-                    headline:
-                        TextStyle(fontSize: 72.0, fontWeight: FontWeight.bold),
-                    title:
-                        TextStyle(fontSize: 36.0, fontStyle: FontStyle.italic),
-                    body1: TextStyle(fontSize: 18.0))),
-            home: RegisterPage()));
+          title: 'Flutter E-Commerce',
+          routes: {
+            '/': (BuildContext context) => ProductsPage(onInit: () {
+                  StoreProvider.of<AppState>(context).dispatch(getUserAction);
+                  StoreProvider.of<AppState>(context)
+                      .dispatch(getProductsAction);
+                }),
+            '/login': (BuildContext context) => LoginPage(),
+            '/register': (BuildContext context) => RegisterPage()
+          },
+          theme: ThemeData(
+              brightness: Brightness.dark,
+              primaryColor: Colors.cyan[400],
+              accentColor: Colors.deepOrange[200],
+              textTheme: TextTheme(
+                  headline:
+                      TextStyle(fontSize: 72.0, fontWeight: FontWeight.bold),
+                  title: TextStyle(fontSize: 36.0, fontStyle: FontStyle.italic),
+                  body1: TextStyle(fontSize: 18.0))),
+        ));
   }
 }
---
+++ lib/pages/product_detail_page.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_ecommerce/models/product.dart';
+import 'package:flutter_ecommerce/pages/products_page.dart';
+
+class ProductDetailPage extends StatelessWidget {
+  final Product item;
+  ProductDetailPage({this.item});
+
+  @override
+  Widget build(BuildContext context) {
+    final String pictureUrl = 'https://flutter-ecommerce-api.herokuapp.com${item.picture['url']}';
+    final Orientation orientation = MediaQuery.of(context).orientation;
+    return Scaffold(
+        appBar: AppBar(title: Text(item.name)),
+        body: Container(
+            decoration: gradientBackground,
+            child: Column(children: [
+              Padding(
+                padding: EdgeInsets.only(bottom: 10.0),
+                child: Hero(
+                    tag: item,
+                    child: Image.network(pictureUrl,
+                        width: orientation == Orientation.portrait ? 600 : 250,
+                        height: orientation == Orientation.portrait ? 400 : 200,
+                        fit: BoxFit.cover)),
+              ),
+              Text(item.name, style: Theme.of(context).textTheme.title),
+              Text('\$${item.price}', style: Theme.of(context).textTheme.body1),
+              Flexible(
+                  child: SingleChildScrollView(
+                      child: Padding(
+                          child: Text(item.description),
+                          padding: EdgeInsets.only(
+                              left: 32.0, right: 32.0, bottom: 32.0))))
+            ])));
+  }
+}
